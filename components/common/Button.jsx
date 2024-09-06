80e4ab2e23ec4a619f0dac0ce416fca8e066e1a0
rozycos <rozycos@gmail.com>
2024-09-07 00:08:03 +0200

    created level sweacher

--- components/common/Button.jsx
+++ components/common/Button.jsx
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
 
-export default function Button({ text = 'start', secondary = false }) {
+export default function Button({ text = 'start', secondary = false, onClick}) {
     const buttonClassName = `btn ${secondary ? 'btn--secondary' : ''}`;
     
     return (
-        <button className={buttonClassName}>
+        <button className={buttonClassName} onClick={onClick}>
             <div className="wrapper dual-color-text">
                 {text}
             </div>
--- components/page-specific/AboutMe/AboutMe.jsx
+++ components/page-specific/AboutMe/AboutMe.jsx
@@ -1,17 +1,27 @@
 "use client"
+import React, { useState, useEffect } from 'react';
 import Button from "@/components/common/Button"
 import LevelLoader from "@/components/common/LevelLoader"
 
 export default function AboutMe () {
+    const [showDone, setShowDone] = useState(false);
+
+    const handleClick = () => {
+      setShowDone(true);
+    };
     return(
         <>
+            {!showDone ? (
             <main className="level-loader level-loader__container">
                 {/* <nav>Nawigation</nav> */}
                 {/* <section className="level-loader"> */}
                     <LevelLoader level='level 10' h1='know about me'/>
-                    <Button text='start' secondary={false}/>
+                    <Button text='start' secondary={false} onClick={handleClick}/>
                 {/* </section> */}
             </main>
+            ) : (
+                <div>HURRAAA!!</div>
+            )}
         </>
     )
 }
