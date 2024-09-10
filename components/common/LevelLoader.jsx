cae581549c21563e958a0f055faae463f56634ea
rozycos <rozycos@gmail.com>
2024-09-10 18:54:52 +0200

    created aboutme page js logic and stying

--- components/common/LevelLoader.jsx
+++ components/common/LevelLoader.jsx
@@ -3,8 +3,8 @@ import Button from "./Button";
 export default function LevelLoader({level='level 1', h1='level name'}) {
     return (
         <>
-            <p className="level-loader__title dual-color-text">{level}</p>
-            <h1>{h1}</h1>
+            <h1 className="level-loader__title dual-color-text">{level}</h1>
+            <h2>{h1}</h2>
             {/* <Button text='start' secondary='true'/> */}
         </>
     )
--- components/page-specific/AboutMe/AboutMe.jsx
+++ components/page-specific/AboutMe/AboutMe.jsx
@@ -1,27 +1,35 @@
 "use client"
-import React, { useState, useEffect } from 'react';
+//import React, { useState } from 'react';
 import Button from "@/components/common/Button"
 import LevelLoader from "@/components/common/LevelLoader"
 
 export default function AboutMe () {
-    const [showDone, setShowDone] = useState(false);
+    //const [showDone, setShowDone] = useState(false);
 
-    const handleClick = () => {
-      setShowDone(true);
-    };
     return(
+        // <>
+        //     {!showDone ? (
+        //     <main className="level-loader level-loader__container">
+        //         {/* <nav>Nawigation</nav> */}
+        //         {/* <section className="level-loader"> */}
+        //             <LevelLoader level='level 1' h1='know about me'/>
+        //             <Button text='start' secondary={false} destinatioPage ='/about-me/hero'/>
+        //         {/* </section> */}
+        //     </main>
+        //     ) : (
+        //         <div>HURRAAA!!</div>
+        //     )}
+        // </>
+
         <>
-            {!showDone ? (
-            <main className="level-loader level-loader__container">
-                {/* <nav>Nawigation</nav> */}
-                {/* <section className="level-loader"> */}
-                    <LevelLoader level='level 1' h1='know about me'/>
-                    <Button text='start' secondary={false} destinatioPage ='/about-me/hero'/>
-                {/* </section> */}
-            </main>
-            ) : (
-                <div>HURRAAA!!</div>
-            )}
-        </>
+        <main className="level-loader level-loader__container">
+            {/* <nav>Nawigation</nav> */}
+            {/* <section className="level-loader"> */}
+                <LevelLoader level='level 1' h1='about me'/>
+                <Button text='start' secondary={false} destinatioPage ='/about-me/hero'/>
+            {/* </section> */}
+        </main>
+ 
+    </>
     )
 }
