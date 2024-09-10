c20a956a83fa34b889b1c0d6f86e94450a0ce31c
rozycos <rozycos@gmail.com>
2024-09-10 22:26:35 +0200

    improved levelLoader logic, created reusable levelLoader component, creating styling for levelLoader

--- components/common/LevelLoader.jsx
+++ components/common/LevelLoader.jsx
@@ -1,13 +1,33 @@
+"use client"
 import Link from "next/link";
 import Button from "./Button";
 
-export default function LevelLoader({level='level 1', levelName='level name'}) {
+export default function LevelLoader({link='/', linkName='home', level='level 1', levelName='level name', destinationPage = '/'}) {
     return (
         <>
 
-                <h1 className="level-loader__heading dual-color-text">{level}</h1>
-                <h2 className="level-loader__subheading">{levelName}</h2>
+                {/* <h1 className="level-loader__heading dual-color-text">{level}</h1>
+                <h2 className="level-loader__subheading">{levelName}</h2> */}
                 {/* <Button text='start' secondary='true'/> */}
+                <main className="level-loader level-loader__container">
+                {/* <nav>Nawigation</nav> */}
+                {/* <section className="level-loader"> */}
+                <section className="level-loader__navigation">
+                    <ul>
+                        <li className="level-loader__list">
+                            {/* <div className="level-loader__list-style"></div> */}
+                            <Link href={link} className="level-loader__list-style dual-color-text__secondary">{linkName}</Link>
+                        </li>
+                    </ul>
+                </section>
+                <section className="level-loader__section">      
+                    {/* <LevelLoader level='level 1' levelName='about me'/> */}
+                    <h1 className="level-loader__heading dual-color-text">{level}</h1>
+                    <h2 className="level-loader__subheading">{levelName}</h2>
+                    <Button text='start' secondary={false} destinationPage ={destinationPage}/>
+                </section>
+                <section></section>
+            </main>
         </>
     )
 }
--- components/page-specific/AboutMe/AboutMe.jsx
+++ components/page-specific/AboutMe/AboutMe.jsx
@@ -7,39 +7,30 @@ import Link from "next/link"
 export default function AboutMe () {
     //const [showDone, setShowDone] = useState(false);
 
-    return(
-        // <>
-        //     {!showDone ? (
-        //     <main className="level-loader level-loader__container">
-        //         {/* <nav>Nawigation</nav> */}
-        //         {/* <section className="level-loader"> */}
-        //             <LevelLoader level='level 1' h1='know about me'/>
-        //             <Button text='start' secondary={false} destinatioPage ='/about-me/hero'/>
-        //         {/* </section> */}
-        //     </main>
-        //     ) : (
-        //         <div>HURRAAA!!</div>
-        //     )}
-        // </>
-
+    return(            
         <>
-            <main className="level-loader level-loader__container">
-                {/* <nav>Nawigation</nav> */}
-                {/* <section className="level-loader"> */}
-                <section>
-                    <ul>
-                        <li>
-                            <div className="level-loader__list-style"></div>
-                            <Link href={'/'}>home</Link>
-                        </li>
-                    </ul>
-                </section>
-                <section>      
-                    <LevelLoader level='level 1' levelName='about me'/>
-                    <Button text='start' secondary={false} destinationPage ='/about-me/hero'/>
-                </section>
-                <section></section>
-            </main>
+            <LevelLoader link='/' linkName='home' level='level 1' levelName='about me' destinationPage = '/about-me/hero'/>
         </>
     )
 }
+
+
+//<main className="level-loader level-loader__container">
+//{/* <nav>Nawigation</nav> */}
+//{/* <section className="level-loader"> */}
+//<section className="level-loader__navigation">
+//    <ul>
+//        <li className="level-loader__list">
+//            {/* <div className="level-loader__list-style"></div> */}
+//            <Link href={'/'} className="level-loader__list-style dual-color-text__secondary">home</Link>
+//        </li>
+//    </ul>
+//</section>
+//<section className="level-loader__section">      
+//    {/* <LevelLoader level='level 1' levelName='about me'/> */}
+//    <h1 className="level-loader__heading dual-color-text">{level}</h1>
+//    <h2 className="level-loader__subheading">{levelName}</h2>
+//    <Button text='start' secondary={false} destinationPage ='/about-me/hero'/>
+//</section>
+//<section></section>
+//</main>
