8e7b7fbf96d51439959d5ce8d885f84c59528046
rozycos <rozycos@gmail.com>
2024-09-11 21:36:28 +0200

    created animated characters in scss

--- app/about-me/hero/page.jsx
+++ app/about-me/hero/page.jsx
@@ -1,10 +1,15 @@
+import Character from "@/components/common/Character";
 import styles from "../../../styles/elements/AboutMe.module.scss";
 
 export default function Hero() {
     return (
         <div className={styles.container}>
             <header></header>
-            <main><h2>Hero!!!</h2></main>
+            <main>
+                <h2>Hero!!!</h2>
+                <Character character="scientist"/>
+                <Character character="astronaut"/>
+            </main>
             <footer className="footer"></footer>
         </div>
     );
