085b1d1c1a1fa35e65ceaad26b05f9133894a6df
rozycos <rozycos@gmail.com>
2024-09-21 22:37:56 +0200

    reorganising scss files: page.scss, Page.module.scss

--- app/about-me/hero/page.jsx
+++ app/about-me/hero/page.jsx
@@ -3,6 +3,7 @@ import styles from "../../../styles/elements/Page.module.scss";
 import Heading from "@/components/common/Heading";
 import TypingText from "@/components/common/TypingText";
 import AstronautImageLink from "@/components/page-specific/AboutMe/AstronautImageLink";
+import Image from "next/image";
 
 export default function Hero() {
     const HeroSectionText = `I am a third year undergrad student studying in SOME Institute of Technology, SOMEWHERE. I am from the City of Joy, CITYNAME. I completed my +2 from SOME SCHOOL.`
@@ -15,13 +16,18 @@ export default function Hero() {
                     <Heading text="I'm Przemek Malec"/>
                 </section>
                 <section className="page__section--information">
-                    <div className="page__wrapper">
-                        <div className="page_image">obrazek</div>
+                    <div className="container page__wrapper">
+                        <div className="page__image">
+                                <div className="page__hero"></div>
+                                {/* <Image width={200} height={200} alt="hero image" src="/images/hero.png"/> */}
+                            
+                        </div>
                         <div className="page__information">
                             <TypingText text={HeroSectionText} typingSpeed={20}/>
                         </div>
-                        <AstronautImageLink/>
+
                     </div>
+                    {/* <AstronautImageLink/> */}
                 </section>
                 {/* <Character character="scientist"/> */}
 
