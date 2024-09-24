bd8be86f75e4fa1ce2390fb2399d5828383abd0f
rozycos <rozycos@gmail.com>
2024-09-24 20:03:02 +0200

    styling hero page

--- components/page-specific/AboutMe/Hero.jsx
+++ components/page-specific/AboutMe/Hero.jsx
@@ -1,12 +1,32 @@
 import TypingText from "@/components/common/TypingText";
+//import Image from "next/image";
 
 export default function Hero () {
     const HeroSectionText = `I am a third year undergrad student studying in SOME Institute of Technology, SOMEWHERE. I am from the City of Joy, CITYNAME. I completed my +2 from SOME SCHOOL.`
+    // const imageStyle = {
+    //     width: 100,
+    //     height: 'auto',
+    //     @media (max-width: 768px){
+    //         width:150,
+    //         height: 'auto'
+    //     }
+        
+    //   }
+    
     return(            
         <>
-            <div className="hero__wrapper">
-                <div className="hero__pic">
-                    
+            <div className="hero__content">
+                <div className="hero__image">
+                    <div className="hero__image-main"></div>
+                    {/* <Image 
+                        fill
+                        //style={styles}
+                        src="/images/hero.png"
+                        //width={150}
+                        //height={150}
+                        sizes="(max-width: 768px) 100px, 100px"
+                        alt="Picture of the author"
+                    /> */}
                 </div>
                 <div className="hero__description">
                     <TypingText text={HeroSectionText} typingSpeed={20}/>
